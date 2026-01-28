a5f4e27f4f478124681883fcc1cc61fc7783f0fb
Stepan csl <cslpnj@gmail.com>
2026-01-28 23:30:02 +0300

    First step - pom.xml, basic project structure

---
+++ src/main/java/org/alekseev/App.java
@@ -0,0 +1,11 @@
+package org.alekseev;
+
+/**
+ * @author stepa
+ * @since 2026-01-28
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("User-service started");
+    }
+}
