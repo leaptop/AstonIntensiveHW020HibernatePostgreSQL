4e328247063319154c7fd24c8f6eedaf446a0523
Stepan csl <cslpnj@gmail.com>
2026-02-01 01:25:50 +0300

    Added Hibernate config. Added DB testing via App. A users table is created in PostgreSQL

--- src/main/java/org/alekseev/App.java
+++ src/main/java/org/alekseev/App.java
@@ -1,11 +1,21 @@
 package org.alekseev;
 
-/**
- * @author stepa
- * @since 2026-01-28
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("User-service started");
+
+        SessionFactory sessionFactory = new Configuration()
+                .configure() // по умолчанию ищет hibernate.cfg.xml в resources
+                .buildSessionFactory();
+
+        try (Session session = sessionFactory.openSession()) {
+            Object result = session.createNativeQuery("select 1").getSingleResult();
+            System.out.println("DB connection OK, select 1 = " + result);
+        } finally {
+            sessionFactory.close();
+        }
     }
 }
